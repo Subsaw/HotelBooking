a92933d4fb42cb6dc7a012990fbd9ea1b4027596
Subsaw <subsaw@leprogrammers.com>
2024-01-05 23:37:04 +0100

    Models + User added

---
+++ HotelBooking/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelBooking.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public virtual Room Room { get; set; }
+
+
+
+    }
+}
---
+++ HotelBooking/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBooking.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public int NumOfPeople { get; set; }
+
+        public float Metric { get; set; }
+
+        public string Description { get; set; }
+    }
+}
